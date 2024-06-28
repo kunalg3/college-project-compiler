a01b3331241e65865486dd8db36f2e01fc0ad27c
kunalg3 <110518293+kunalg3@users.noreply.github.com>
2024-06-28 21:47:02 +0530

    setup .env

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 function Home() {
   return (
     <div>
-        <div className='flex justify-center items-center h-screen text-2xl text-5xl font-bold text-white'>
+        <div className='flex justify-center items-center h-screen text-2xl text-5xl font-bold text-black'>
         Enter the World of Coding-
         <Link to="/login" className='mr-8'> Login</Link>
-        <Link to="/register"> Registar</Link>
+        <Link to="/register"> Register</Link>
         </div>
     </div>
   )
